3abcf6af6636b83db726c79334fb38c633fd5e4d
Bernardolagniton3 <115129935+Bernardolagniton3@users.noreply.github.com>
2022-10-10 14:41:48 +0800

    Add files via upload

--- src/components/user/Login.js
+++ src/components/user/Login.js
@@ -1,10 +1,10 @@
-import React, { Fragment, useState, useEffect } from 'react'
-
-import MetaData from '../layout/MetaData'
+import React, { Fragment, useState ,useEffect} from 'react'
+import { Link } from 'react-router-dom'
 import { gql, useLazyQuery } from '@apollo/client'
 import { useNavigate } from 'react-router-dom' 
 import useLocalStorage from '../../hooks/setlocalstorage'
-
+import MetaData from '../layout/MetaData'
+import Header from '../layout/Header'
 const allResult = gql `
 query ExampleQuery($username: String, $password: String) {
   
@@ -18,21 +18,21 @@ query ExampleQuery($username: String, $password: String) {
         firstname
         lastname
         mobile
-        address
+        title
         location
         balanceRecord
         payment
         datecreation
         gender
-        age
         birthday
-        job
         record
+        fullname
+        phoneNumber
+        emailAddress
       }
     }
   }
 `;
-
 
 const Login = () => {
     const navigate = useNavigate();
@@ -62,9 +62,9 @@ const Login = () => {
     }
     
 },[data, username, password, userRecord]) 
-    console.log("ueser", userRecord)
-   
-    const LogOtp = ({mobile}) => {
+
+  
+const LogOtp = ({mobile}) => {
     const [mobileNumber, setMobileNumber] = useState(mobile);
     const submitHandlerOtp = (e) => {
         e.preventDefault();   
@@ -79,7 +79,7 @@ const Login = () => {
           
              
             <div className="form-group">
-                <label className="labelspan">Cell number</label>
+                
                 <input
                     type="name"
                     id="otp_field"
@@ -108,85 +108,54 @@ const Login = () => {
 
     return (
         <Fragment>
-                <Fragment>
-                    <MetaData title={'Login'} />
-                    <div className="row wrapper">
-                           {show ? (<>
-                          {userRecord.map((item, id)=>(
+            <Fragment>
+                <MetaData title={'Login'} />
+                <Header/>
+                <div className="row wrapper">
+                    {show ? (<> {userRecord.map((item, id)=>(
                                <LogOtp key={id} data={item} mobile={item.mobile} />
-                          ))} 
-                          
-                         {/*    
-                            <div className="col-10 col-lg-5">
-                                <form className="shadow-lg" onSubmit={submitHandlerOtp} encType='multipart/form-data'>
-                                    <h1 className="mb-3">{mobile}Login with an OTP</h1>
-                                  
-                                     
-                                    <div className="form-group">
-                                        <label className="labelspan">Cell number or Email/Username</label>
-                                        <input
-                                            type="name"
-                                            id="otp_field"
-                                            className="form-control"
-                                            value={otpvalue}
-                                            onChange={(e) => setOtpvalue(e.target.value)}
-                                        />
-                                    </div>
-                                    <div className="row"> 
-                                    
-                                    <div className="col-6"> 
+                          ))} </>)
+                          :
+                    <div className="col-10 col-lg-5">
+                        <form className="shadow-lg p-3 mb-5 bg-white rounded" onSubmit={submitHandler}>
+                            <h1 className="mb-3">Login</h1>
+                            <div className="form-group">
+                                <label className="labelspan" htmlFor="email_field">Cell Number or email/username</label>
+                                <input
+                                    type="text"
+                                    id="email_field"
+                                    className="form-control"
+                                    value={username}
+                                    onChange={(e) => setUsername(e.target.value)}
+                                />
+                            </div>
+
+                            <div className="form-group">
+                                <label className="labelspan" htmlFor="password_field">Password</label>
+                                <input
+                                    type="password"
+                                    id="password_field"
+                                    className="form-control"
+                                    value={password}
+                                    onChange={(e) => setPassword(e.target.value)}
+                                />
+                            </div>
+                            <div className='row'>
+                                <div className="col-6">
                                     <button
-                                        id="register_button"
+                                        id="login_button"
                                         type="submit"
                                         className="btn btn-block py-3"
-                                       
                                     >
-                                        Send OTP
+                                        OK
                                     </button>
-                                    </div>
-                                    </div>
-                                </form>
+                                </div>
                             </div>
-                          */}
-                           </>):
-                            <div className="col-10 col-lg-5">
-                            <form className="shadow-lg p-3 mb-5 bg-white rounded" onSubmit={submitHandler}>
-                                <h1 className="mb-3">Authentication</h1>
-                                <div className="form-group">
-                                    <span className="labelspan" htmlFor="email_field">Cell Number or email/username</span>
-                                    <input
-                                        type="text"
-                                        id="email_field"
-                                        className="form-control"
-                                        value={username}
-                                        onChange={(e) => setUsername(e.target.value)}
-                                    />
-                                </div>
-                                <div className="form-group">
-                                <span className="labelspan" htmlFor="password_field">Password</span>
-                                    <input
-                                        type="password"
-                                        id="password_field"
-                                        className="form-control"
-                                        value={password}
-                                        onChange={(e) => setPassword(e.target.value)}
-                                    />
-                                </div>
-                                <div className='row'>
-                                <div className="col-6"> 
-                                <button
-                                    id="login_button"
-                                    type="submit"
-                                    className="btn btn-block py-3">
-                                    LOGIN
-                                </button>
-                                </div>
-                                </div>  
-                            </form>
-                        </div>
-                        }   
+                        </form>
                     </div>
-                </Fragment>         
+                       }
+                </div>
+            </Fragment>
         </Fragment>
     )
 }
