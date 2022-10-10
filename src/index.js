a729808b61639a98d4d55fab548eb2a0a47529f8
Bernardolagniton3 <115129935+Bernardolagniton3@users.noreply.github.com>
2022-10-10 09:40:49 +0800

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -3,12 +3,23 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import {BrowserRouter} from 'react-router-dom'
+import { ApolloClient, InMemoryCache, ApolloProvider,createHttpLink } from "@apollo/client";
+ 
 
+const link = createHttpLink({
+  uri: "http://localhost:4000"
+});
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: link
+})
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
+    <ApolloProvider client={client}>
     <App />
+    </ApolloProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
