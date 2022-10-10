4b58b7ddf582e795dbeee4ddd298fed95121dc12
Bernardolagniton3 <115129935+Bernardolagniton3@users.noreply.github.com>
2022-10-10 14:42:42 +0800

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import { Route, Routes } from 'react-router-dom' 
-import Header from './components/layout/Header'
 import Home from './components/Home'
 import Login from './components/user/Login'
 import Profile from './components/user/Profile'  
@@ -7,7 +6,7 @@ import Profile from './components/user/Profile'
 function App() {
   return (
     <>
-     <Header/>
+     
      <Routes> 
       <Route path="/home" element={<Home/>}/>
       <Route path="/" element={<Login/>}/>
